11097ecf616165102bb5884a60975b82ab0ad25f
Mtvy <m.d.prudnik@gmail.com>
2023-05-04 22:09:51 +0300

    INIT

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"money.tracker"},{"l":"money.tracker.component"},{"l":"money.tracker.domain"},{"l":"money.tracker.repo"},{"l":"money.tracker.view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"money.tracker"},{"l":"money.tracker.component"},{"l":"money.tracker.entity"},{"l":"money.tracker.repo"},{"l":"money.tracker.view"}];updateSearchResults();
